352efed5f8c727e22935c82025c022f657ec5bdd
nuubis <marttikakk@hotmail.com>
2017-05-21 23:19:21 +0300

    One controller stays

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,7 @@
 package application;
-	
-import java.io.IOException;
 
-import game.GameController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,8 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-    	GameController gameController;
-    	loadControls("/ui/ActivityChoice.fxml", new GameController());
         Parent root = FXMLLoader.load(getClass().getResource("/ui/StartMenu.fxml"));
         primaryStage.setTitle("Lost Rage");
         primaryStage.setScene(new Scene(root, 1200, 800));
@@ -28,14 +21,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    private Node loadControls(String fxml, Initializable controller) throws IOException {
-           //URL resource = getClass().getResource(fxml);
-           if (getClass().getResource(fxml) == null)
-               throw new IllegalArgumentException(fxml + " not found");
-
-           FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxml));
-           fxmlLoader.setController(controller);
-           return fxmlLoader.load();
-           
-       }
 }
