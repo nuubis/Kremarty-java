ba5af1d0531eb2437540564065d44bfde0325086
nuubis <marttikakk@hotmail.com>
2017-05-21 16:33:59 +0300

    Organizing

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/StartMenu.fxml"));
         primaryStage.setTitle("Lost Rage");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
--- src/application/RageController.java
+++ src/application/RageController.java
@@ -1,26 +1,49 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class RageController implements Initializable {
 	
 	@FXML
-	public Button play;
+	public Button play, back;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("System working!");
 	}
 	
-	@FXML
-	public void kontroll(){
-		System.out.println("I work!");
-	}
+	 @FXML
+	 private void changeScene(ActionEvent event) throws IOException{
+		 System.out.println("I work");
+	     Stage stage; 
+	     Parent root;
+	     if(event.getSource()==play){
+	        //get reference to the button's stage         
+	        stage=(Stage) play.getScene().getWindow();
+	        //load up OTHER FXML document
+	  root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
+	      }
+	     else{
+	       stage=(Stage) back.getScene().getWindow();
+	  root = FXMLLoader.load(getClass().getResource("/ui/StartMenu.fxml"));
+	      }
+	     //create a new scene with root and set the stage
+	      Scene scene = new Scene(root);
+	      stage.setScene(scene);
+	      stage.show();
+	    }
+	
 	
 	
 }
