f93166ba37960d4b4d392fc576dd87f3dde4fa87
nuubis <marttikakk@hotmail.com>
2017-05-21 15:57:26 +0300

    Play works

--- src/application/RageController.java
+++ src/application/RageController.java
@@ -3,20 +3,23 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 public class RageController implements Initializable {
-
+	
+	@FXML
 	public Button play;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		System.out.println("System working!");
 	}
 	
+	@FXML
 	public void kontroll(){
-		System.out.println("Ma töötan");
+		System.out.println("I work!");
 	}
 	
 	
