7668e2645900fd4e51de548d7e3a47ace6588590
nuubis <marttikakk@hotmail.com>
2017-05-21 14:53:48 +0300

    FIXED

---
+++ src/application/RageController.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class RageController implements Initializable {
+
+	public Button play;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	/*private void kontroll(){
+		System.out.println("Ma töötan");
+	}*/
+	
+	
+}
