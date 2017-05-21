daccd4628c72c42b0349b69b1821e87b2c38d0c0
nuubis <marttikakk@hotmail.com>
2017-05-21 22:21:52 +0300

    quit button

--- src/application/RageController.java
+++ src/application/RageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,39 +12,63 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class RageController implements Initializable {
 	
 	@FXML
-	public Button play, back;
+	public Button play, load, help, quit, back, startGame;
+	public TextField insertName;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		System.out.println("System working!");
 	}
 	
-	 @FXML
-	 private void changeScene(ActionEvent event) throws IOException{
-		 System.out.println("I work");
-	     Stage stage; 
-	     Parent root;
-	     if(event.getSource()==play){
-	        //get reference to the button's stage         
-	        stage=(Stage) play.getScene().getWindow();
-	        //load up OTHER FXML document
-	        root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
-	     }
-	     else{ // if (event.getSource()==back)
-	       stage=(Stage) back.getScene().getWindow();
-	       root = FXMLLoader.load(getClass().getResource("/ui/StartMenu.fxml"));
-	     }
-	     //create a new scene with root and set the stage
-	     Scene scene = new Scene(root);
-	     stage.setScene(scene);
-	     stage.show();
-	     }
+	@FXML
+	private void changeScene(ActionEvent event) throws IOException{
+		System.out.println("I work");
+	    Stage stage; 
+	    Parent root;
+	    if(event.getSource()==play){
+	       //get reference to the button's stage         
+	       stage=(Stage) play.getScene().getWindow();
+	       //load up OTHER FXML document
+	       root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
+	    }
+	    
+	    
+	    else if (event.getSource()==load){
+	    	stage=(Stage) load.getScene().getWindow();
+		    root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
+	    }
+	    
+	    else if (event.getSource()==help){
+	    	stage=(Stage) help.getScene().getWindow();
+		    root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
+	    }
+	    
+	    else if(event.getSource()==startGame){
+	    	stage=(Stage) help.getScene().getWindow();
+		    root = FXMLLoader.load(getClass().getResource("/ui/CreateCharacter.fxml"));
+	    }
+	    	    
+	    else {
+	    	stage=(Stage) back.getScene().getWindow();
+		    root = FXMLLoader.load(getClass().getResource("/ui/StartMenu.fxml"));
+		    }
+	    //create a new scene with root and set the stage
+	    Scene scene = new Scene(root);
+	    stage.setScene(scene);
+	    stage.show();
+	    }
 	
-	
+	@FXML
+	private void quitGame(){
+		
+		Platform.exit();
+    	System.exit(0);	    
+	}
 	
 }
